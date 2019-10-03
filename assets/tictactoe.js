ee360262d6d14a3fc5ba67f4c5808d1dd73f822e
Henry <henrstep@amazon.com>
2019-10-03 10:09:20 -0400

    update files

--- assets/tictactoe.js
+++ assets/tictactoe.js
@@ -1,14 +1,19 @@
 (function(){
     // console.log("hey");
 
-    let currentPlayer = 'X';
+    let currentPlayer = "X";
     let playerXSelections = [];
     let playerOSelections = [];
 
     const winningCombinations = [
         [1, 2, 3],
         [4, 5, 6],
-        [7, 8, 9]
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
     ];
 
     const cellElementArray = document.querySelectorAll('td')
@@ -18,6 +23,27 @@
         currentCell.addEventListener('click', function(event){
             const clickedCellElement = event.target;
             console.log("You clicked on cell number: " + clickedCellElement.id)
+
+            if(currentPlayer === "X"){
+                clickedCellElement.innerHTML = "X";
+                currentPlayer = "O"
+            }else {
+                clickedCellElement.innerHTML = "O"
+                currentPlayer = "X"
+            }
+
+            
+
+
+
+
+
+
+
+
+
+
+
         });
     }
 
